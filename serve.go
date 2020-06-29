2219d99ec5b778f4f88fa9b0cbebbf56226a06e2
mark.ahn <ahn8202@gmail.com>
2020-06-30 00:37:44 +0900

    adds thread server

--- serve.go
+++ serve.go
@@ -9,6 +9,10 @@ type ThreadServer interface {
 	ServeThread(ctx context.Context, breaker ContextBreak)
 }
 
+type ThreadServerFunc func(context.Context, ContextBreak)
+
+func (__ ThreadServerFunc) ServeThread(ctx context.Context, bk ContextBreak) { __(ctx, bk) }
+
 func Serve(ctx context.Context, server ThreadServer) SrvHandle {
 	in_ctx, cancel := context.WithCancel(ctx)
 	in_ctx, done := WithThreadDoneNotify(in_ctx, &sync.WaitGroup{})
