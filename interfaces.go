c9d251634b11a1b3d6a60248b1df0c3babb59c56
mark.ahn <ahn8202@gmail.com>
2022-12-14 15:24:43 +0900

    adds CloseFunc

--- interfaces.go
+++ interfaces.go
@@ -5,3 +5,11 @@ import "context"
 type Closer interface {
 	Close(ctx context.Context) error
 }
+
+var _ Closer = CloseFunc(nil)
+
+type CloseFunc func(ctx context.Context) error
+
+func (__ CloseFunc) Close(ctx context.Context) error {
+	return __(ctx)
+}
