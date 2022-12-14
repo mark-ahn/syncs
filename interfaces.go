860fe09bdd05aebe863cf91ffa7a6672b96f37ec
mark.ahn <ahn8202@gmail.com>
2022-12-14 15:34:29 +0900

    adds Procedure

--- interfaces.go
+++ interfaces.go
@@ -1,6 +1,9 @@
 package syncs
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type Closer interface {
 	Close(ctx context.Context) error
@@ -8,8 +11,20 @@ type Closer interface {
 
 var _ Closer = CloseFunc(nil)
 
-type CloseFunc func(ctx context.Context) error
+type Procedure func(ctx context.Context) error
+
+func (__ Procedure) CallWithTimeout(ctx context.Context, du time.Duration) error {
+	return CallWithTimeout(ctx, du, __)
+}
+
+type CloseFunc Procedure
 
 func (__ CloseFunc) Close(ctx context.Context) error {
 	return __(ctx)
+}
+
+func CallWithTimeout(ctx context.Context, du time.Duration, f Procedure) error {
+	ctx, stop := context.WithTimeout(ctx, du)
+	defer stop()
+	return f(ctx)
 }
